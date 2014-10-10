8191b1dfed942edcb933dbd7eca8208b2d0cef85
Shangoman <shangoman@outlook.com>
2014-10-10 05:07:57 +0100

    Bring in puzzle builder component

---
+++ KrossBuilder/Cell.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace KrossBuilder
+{
+    public class Cell
+    {
+        public int Row { get; set; }
+        public int Col { get; set; }
+        public string Character { get; set; }
+        public bool IsFirstLetter { get; set; }
+        public bool IsJunction { get; set; }
+        public Tuple<int, int> HorizontalPreceedingRelative { get; set; }
+        public Tuple<int, int> VerticalPreceedingRelative { get; set; }
+        public string[] WordH { get; set; }
+        public int IndexH { get; set; }
+        public int IndexV { get; set; }
+        public string[] WordV { get; set; }
+    }
+}
---
+++ KrossBuilder/InsertWordResult.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace KrossBuilder
+{
+    public class InsertWordResult
+    {
+        public bool IsVertical { get; set; }
+        public Tuple<int, int> StartCell { get; set; }
+        public Tuple<int, int> EndCell { get; set; }
+        public bool Inserted { get; set; }
+        public string[] Word { get; set; }
+    }
+}
